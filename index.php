7327caa2e798ed5c1412936470816d97c7a9d0e5
Alípio Afonso de Almeida Oliveira <aolazulum@hotmail.com>
2016-05-17 15:25:08 -0300

    Iniciei o projeto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Meu Coração</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div id="wrapper">
+		<div id="frase">Veja o que está dentro do meu coração</div>
+		<div id="coracao1" class="coracao"></div>
+		<div id="coracao2" class="coracao"></div>
+	</div>
+
+</body>
+</html>
